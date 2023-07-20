02d4556ef00500be98fed326a7ced67982524f2a
Joseph Kilunda <kilundajoseph68@gmail.com>
2023-07-20 12:45:05 +0300

    first commit

---
+++ first.js
@@ -0,0 +1,263 @@
+// console.log("hello world")
+
+
+// //variables
+// num = 2+2 
+// console.log(num)
+// console.log (num + 3)
+// let username = "Kilunda's account number"
+// console.log(username)
+
+
+// //constants
+// let radius = 5
+// const pi = 3.14
+// let area 
+// area = pi * radius * radius
+// console.log (area)
+
+
+// //data types 
+// let num1 = 5/0
+// console.log(num1)
+// let user = "kilunda"
+// console.log(user)
+// let firstname = "Joseph"
+// let lastname = "Kilunda"
+// console.log(firstname + " "+ lastname)
+// let bool = 1>2
+// console.log(typeof bool)
+
+
+
+// // type conversion
+// let num2 = String(6)
+// console.log (num2)
+// console.log (typeof num2)
+
+
+// //operator
+// let num5 = 4
+// let num4 = 56
+// let result = num5 + num4
+// console.log(result)
+
+// let num6 = 5
+// let x = num6++   //post increament
+// console.log(x, num6)
+
+// console.log( 5>6)
+
+
+
+// //logical operators
+// let x = 7, y = 8, z = 9
+// let result = x < y && x < z
+// console.log(result)
+
+
+
+//  conditions
+// let num1 = 9
+// let num2 = 7
+
+// result = num1 < num2
+// if(result)
+//     console.log("Num1 is greater")
+// else
+//     console.log("Num2 is greater")
+// console.log("bye")
+  
+
+// let num1 = 9
+// let num2 = 7
+// let num3 = 4
+
+// if(num1 > num2 && num1 > num3)
+//     console.log("num1 is greatest")
+
+// else if(num2 > num3 && num2 > num3)
+//     console.log("num2 is greatest")
+
+// else
+//     console.log("Num 3is greatest")
+
+
+
+    //switch statements
+
+// let day ="Saturday"
+// switch (day){
+//     case 'Monday':
+//         console.log("7am");
+//         break
+//     case 'Tuesday':
+//         console.log("4am");
+//         break
+//     case 'MWednesday':
+//         console.log("4am");
+//         break
+//      case 'Thursday':
+//         console.log("4am");
+//         break
+//     case 'Friday':
+//         console.log("9am");
+//         break
+//     case 'Saturday':
+//         console.log("8am");
+// }
+
+
+
+//Template literal
+// let num5 = 4
+// let num4 = 56
+// let result = num5 + num4
+// console.log('the ');
+// console.log('The addition of ${num5} and ${num4} is ${result}')
+
+
+
+// let num1 = 3
+// let num2 = 4
+// let result = num1 + num2
+// console.log('the ');
+
+
+
+
+
+// loops
+        // while loop
+// let i = 10
+
+// while(i<=5)
+// {
+//     console.log("Hi");
+//     i++
+// }
+
+
+        //do while
+// let i = 4
+
+// do
+// {
+//     console.log ("Hi", i )
+//     i++
+// }
+// while(i<=5)
+
+
+        //  for loop
+// for(let i=4; i<=5; i++)
+// {
+//     console.log("Hi");
+// }
+ 
+
+
+    //Ojects in Javascript
+// let input= 'name'
+// let kilunda = {
+// name: 'Joseph' , age : 23, county: {county1:'Nakuru',
+// county2: 'kitui',
+// county3: 'kakamega'
+// }
+// }
+// // delete kilunda.county
+// // console.log( kilunda.age);
+// // console.log(kilunda[input]);
+// // console.log(kilunda.county.county2);
+// // console.log(kilunda);
+
+// for (let key in kilunda)
+// {
+//     console.log(key, kilunda[key]);
+// }
+
+
+
+        //FUNCTIONS
+
+// function greet ()
+// {
+//     console.log("hello world");
+// }
+// greet ()
+// console.log("1");
+// greet ()
+// greet ()
+// greet ()
+
+
+        //returning values
+    // function greet ()
+    // {
+    //     return "Hello world"
+    // }
+    // let str = greet ()
+    // console.log(str);
+
+
+
+           //passing values
+//     function greet (user)
+//     {
+//         return 'Hello (user)!!'
+//     }
+
+//     let user = 'Joe';
+//     let str = greet(user);
+//     console.log(str);
+
+
+        // fuction expression
+// let add = function (num1 , num2)
+// {
+//         return num1 + num2
+// }
+// let result = add (5,6)
+// console.log (result)
+
+
+        //methods  they are functions
+        // Arrays
+ 
+        
+ 
+                // constructors
+// function Alien (name, tech,working){
+//         this.name = name
+//         this.tech =tech
+//         this.working = working
+//         this.work = function (){
+//                 console.log("debugging");
+             
+//         }
+//         return 7
+// }
+// let alien1 = new Alien ('kilunda', 'Js','training')
+// let alien2 = new Alien ('Joe','Engineer')
+
+// alien1.tech ='nodejs'
+
+// console.log(alien1, alien2);
+
+        //factorial
+// function fact (n){
+//         if(n==0)
+//                 return 1
+//         else
+//                 return n * fact(n-1)
+
+// }
+// let num =5;
+// let result = fact(num);
+// console.log(result);
+
+// let num = Math.round (2.8)
+// console.log(num);
+
+
+  
